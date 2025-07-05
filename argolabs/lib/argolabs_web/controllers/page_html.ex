c9ca8fb321fd00f1b444fb30e91466c73f36382e
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2025-07-05 23:20:07 +0700

    Initial ArgoCD and sample application
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

---
+++ argolabs/lib/argolabs_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule ArgolabsWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use ArgolabsWeb, :html
+
+  embed_templates "page_html/*"
+end
